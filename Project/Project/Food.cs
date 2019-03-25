ddf7df87b8d0720f48d0dc00487f72e586728d6d
Iván Rodríguez <irofer96@gmail.com>
2019-03-25 14:03:33 +0100

    Meals

---
+++ Project/Project/Food.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project
+{
+    public class Food
+    {
+        public int id = 0;
+        public DateTime realTime;
+        public enum Type
+        {
+            DESAYUNO,
+            COMIDA,
+            MERIENDA,
+            CENA,
+            OTROS
+        }
+        public Type food;
+        public int totalKCal;
+
+        public Food(int id, DateTime realTime, Type food, int totalKCal)
+        {
+            this.id = id;
+            this.realTime = realTime;
+            this.food = food;
+            this.totalKCal = totalKCal;
+        }
+    }
+}
---
+++ Project/Project/Meal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project
+{
+    public class Meal
+    {
+        public int id = 0;
+        public string name = "";
+        public int kCal;
+        public int carbohydrates = 0;
+        public int proteins = 0;
+        public int grease = 0;
+        public int salt = 0;
+
+        public Meal(int id, string name, int kCal, int carbohydrates, int proteins, int grease, int salt)
+        {
+            this.id = id;
+            this.name = name;
+            this.kCal = kCal;
+            this.carbohydrates = carbohydrates;
+            this.proteins = proteins;
+            this.grease = grease;
+            this.salt = salt;
+        }
+    }
+}

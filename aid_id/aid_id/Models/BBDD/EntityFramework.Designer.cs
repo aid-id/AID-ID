11e50dc9f3168c704dde8d974ae362cc596cdabf
SrUrn <ianeito95@gmail.com>
2019-04-16 12:00:51 +0200

    Revert "Merge"
    This reverts commit 3b3eb43d226f1708bda5ffa23891f8604bdf6fb4, reversing
    changes made to da90ca617116c6e2ae7750f7ff2a8254dcc49e16.

--- aid_id/aid_id/Models/BBDD/EntityFramework.Designer.cs
+++ aid_id/aid_id/Models/BBDD/EntityFramework.Designer.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Mati.DESKTOP-QU0511T\Documents\GitHub\AID-ID\aid_id\aid_id\Models\BBDD\EntityFramework.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
@@ -9,15 +8,3 @@
 // para el modelo, abra el modelo en el diseñador, haga clic con el botón secundario en la superficie del diseñador y
 // seleccione 'Actualizar modelo desde base de datos...', 'Generar base de datos desde modelo...' o 'Agregar elemento de generación
 // de código...'.
-=======
-﻿// T4 code generation is enabled for model 'C:\Users\mati\Desktop\AID-ID\aid_id\aid_id\Models\BBDD\EntityFramework.edmx'. 
-// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
-// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
-// is open in the designer.
-
-// If no context and entity classes have been generated, it may be because you created an empty model but
-// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
-// classes for your model, open the model in the designer, right-click on the designer surface, and
-// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
-// Item...'.
->>>>>>> 9beb18f2d79b1691fbc13ad8bd6eba1c16362b63

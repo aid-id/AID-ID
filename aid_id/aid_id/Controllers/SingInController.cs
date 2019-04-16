db289f0faa75a241e9fc721a26ca60518faab1a3
Alistair <alistair.mova@gmail.com>
2019-04-16 09:21:16 +0200

    Vistas SingInUp

---
+++ aid_id/aid_id/Controllers/SingInController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aid_id.Controllers
+{
+    public class SingInController : Controller
+    {
+        // GET: SingIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ aid_id/aid_id/Controllers/SingUpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aid_id.Controllers
+{
+    public class SingUpController : Controller
+    {
+        // GET: SingUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

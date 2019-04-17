991fe2f89642483b1cdf8631febc9c76c6a0d89b
SrUrn <ianeito95@gmail.com>
2019-04-18 00:18:08 +0200

    Modelos listos para migrar

--- aid_id/aid_id/Models/Analisis.cs
+++ aid_id/aid_id/Models/Analisis.cs
@@ -1,41 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace aid_id.Models.BBDD
 {
-
-    
-public partial class Analisis
-{
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-    public Analisis()
+    public partial class Analisis
     {
+        // Primary key. Entity Framework siempre buscara por una palabra que contenga ID
+        public long id_analisis { get; set; }
 
-        this.Comidas = new HashSet<Comidas>();
+        // Creacion de todos los campos de la tabla
+        public byte valor { get; set; }
 
+        [DataType(DataType.Date)]
+        public DateTime fecha_hora { get; set; }
+
+        public byte intensidad_deporte { get; set; }
+
+        public long id_usuario { get; set; }
+
+        // "Collection navigation property" de tipo comidas
+        public virtual ICollection<Comidas> Comidas { get; set; }
+
+        // "Navigation property". Foreign key de la tabla usuarios
+        public virtual Usuarios Usuarios { get; set; }
     }
-
-
-    public long id_analisis { get; set; }
-
-    public byte valor { get; set; }
-
-    public System.DateTime fecha_hora { get; set; }
-
-    public Nullable<byte> intensidad_deporte { get; set; }
-
-    public long id_usuario { get; set; }
-
-
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-
-    public virtual ICollection<Comidas> Comidas { get; set; }
-
-    public virtual Usuarios Usuarios { get; set; }
-
 }
 
 

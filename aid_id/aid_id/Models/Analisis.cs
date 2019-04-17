5debfa7f2cd641258f3c4e80d03d85ce65b34bbc
SrUrn <ianeito95@gmail.com>
2019-04-17 14:02:29 +0200

    eliminación de EF
    puede dar fallos, no esta terminado, no os alarmeis

--- aid_id/aid_id/Models/Analisis.cs
+++ aid_id/aid_id/Models/Analisis.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace aid_id.Models
+
+namespace aid_id.Models.BBDD
 {
-    public class Analisis
+
+    
+public partial class Analisis
+{
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+    public Analisis()
     {
 
+        this.Comidas = new HashSet<Comidas>();
+
     }
+
+
+    public long id_analisis { get; set; }
+
+    public byte valor { get; set; }
+
+    public System.DateTime fecha_hora { get; set; }
+
+    public Nullable<byte> intensidad_deporte { get; set; }
+
+    public long id_usuario { get; set; }
+
+
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+
+    public virtual ICollection<Comidas> Comidas { get; set; }
+
+    public virtual Usuarios Usuarios { get; set; }
+
 }
+
+

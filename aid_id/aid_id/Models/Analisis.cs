0e26094404e5fea33d4ae03dfc6f96f73652ccfc
Alistair <alistair.mova@gmail.com>
2019-04-18 10:14:04 +0200

    Revert "Merge branch 'master' of https://github.com/aid-id/AID-ID"
    This reverts commit 794c588375876a57e0b512a8f0033ebd9fd2cfb2, reversing
    changes made to e50a979f6ca7c85512f753a6e96d49fad4a4c12c.

--- aid_id/aid_id/Models/Analisis.cs
+++ aid_id/aid_id/Models/Analisis.cs
@@ -1,31 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
 
-
-namespace aid_id.Models.BBDD
+namespace aid_id.Models
 {
-    public partial class Analisis
+    public class Analisis
     {
-        // Primary key. Entity Framework siempre buscara por una palabra que contenga ID
-        public long id_analisis { get; set; }
 
-        // Creacion de todos los campos de la tabla
-        public byte valor { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime fecha_hora { get; set; }
-
-        public byte intensidad_deporte { get; set; }
-
-        public long id_usuario { get; set; }
-
-        // "Collection navigation property" de tipo comidas
-        public virtual ICollection<Comidas> Comidas { get; set; }
-
-        // "Navigation property". Foreign key de la tabla usuarios
-        public virtual Usuarios Usuarios { get; set; }
     }
 }
-
-

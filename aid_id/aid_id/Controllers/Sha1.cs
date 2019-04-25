669f879eaa83a7298d926b7d19d19b7fe755d8b9
David <eldeividlh@gmail.com>
2019-04-25 10:39:04 +0200

    Login with database

---
+++ aid_id/aid_id/Controllers/Sha1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+// https://adeshoras.wordpress.com/2008/06/23/generando-un-hash-sha1-con-aspnet-y-c-de-forma-sencilla-y-simple/
+namespace aid_id.Controllers
+{
+    class Sha1
+    {
+        public string GetSHA1(String texto)
+        {
+            SHA1 sha1 = SHA1CryptoServiceProvider.Create();
+            Byte[] textOriginal = ASCIIEncoding.Default.GetBytes(texto);
+            Byte[] hash = sha1.ComputeHash(textOriginal);
+            StringBuilder cadena = new StringBuilder();
+            foreach (byte i in hash)
+            {
+                cadena.AppendFormat("{0:x2}", i);
+            }
+            return cadena.ToString();
+        }
+    }
+}

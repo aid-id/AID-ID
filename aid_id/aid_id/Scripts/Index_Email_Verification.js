1de1f04cda9dc8875607f0794a52824b2f024af4
David Bernal Gonzalez <eldeividlh@gmail.com>
2019-04-29 22:50:25 +0200

    Edito fallo en el JS > Index Email Verification y añado el LogOut al menu de usuario

--- aid_id/aid_id/Scripts/Index_Email_Verification.js
+++ aid_id/aid_id/Scripts/Index_Email_Verification.js
@@ -1,18 +1,17 @@
 ﻿$(document).ready(function () {
-    //global vars
     $("#IndexForm").submit(function () {
-        if ($("#inputEmail").val().length == 0) {
+        if ($("#inputEmailIndex").val().length == 0) {
             document.getElementById("inputEmailIndex").style.borderColor = "red";
             document.getElementById("inputEmailIndex").classList.add("input-text-error-class");
             document.getElementById("inputEmailIndex").placeholder = "Error! Email vacío";
             return false;
         }
-        else if ($("#inputEmail").val().length > 0) {
-            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username.val())) {
+        else if ($("#inputEmailIndex").val().length > 0) {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($("#inputEmailIndex").val())) {
                 document.getElementById("inputEmailIndex").classList.remove("input-text-error-class");
                 return true;
             } else {
-                $('#inputEmail').each(function () {
+                $('#inputEmailIndex').each(function () {
                     $(this).val("");
                 });
                 document.getElementById("inputEmailIndex").style.borderColor = "red";

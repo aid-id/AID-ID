fb404d35e44cb0c2d2a59220eafb38283e43fc7d
David Bernal Gonzalez <eldeividlh@gmail.com>
2019-04-29 17:30:53 +0200

    Bloqueo de Inputs Email Vía CSS + Limpiar JS

--- aid_id/aid_id/Scripts/Index_Email_Verification.js
+++ aid_id/aid_id/Scripts/Index_Email_Verification.js
@@ -1,26 +1,23 @@
 ﻿$(document).ready(function () {
     //global vars
-    var form = $("#IndexForm");
-    var username = $("#inputEmail");
-    var re = /\S+@@\S+\.\S+/;
-    form.submit(function () {
-        if (username.val().length == 0) {
-            document.getElementById("inputEmail").style.borderColor = "red";
-            document.getElementById("inputEmail").classList.add("input-text-error-class");
-            document.getElementById("inputEmail").placeholder = "Error! Email vacío";
+    $("#IndexForm").submit(function () {
+        if ($("#inputEmail").val().length == 0) {
+            document.getElementById("inputEmailIndex").style.borderColor = "red";
+            document.getElementById("inputEmailIndex").classList.add("input-text-error-class");
+            document.getElementById("inputEmailIndex").placeholder = "Error! Email vacío";
             return false;
         }
-        else if (username.val().length > 0) {
+        else if ($("#inputEmail").val().length > 0) {
             if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username.val())) {
-                document.getElementById("inputEmail").classList.remove("input-text-error-class");
+                document.getElementById("inputEmailIndex").classList.remove("input-text-error-class");
                 return true;
             } else {
                 $('#inputEmail').each(function () {
                     $(this).val("");
                 });
-                document.getElementById("inputEmail").style.borderColor = "red";
-                document.getElementById("inputEmail").classList.add("input-text-error-class");
-                document.getElementById("inputEmail").placeholder = "Error! Email no valido";
+                document.getElementById("inputEmailIndex").style.borderColor = "red";
+                document.getElementById("inputEmailIndex").classList.add("input-text-error-class");
+                document.getElementById("inputEmailIndex").placeholder = "Error! Email no valido";
                 return false;
             }
         }
